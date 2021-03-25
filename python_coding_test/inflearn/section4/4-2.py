541e53f65dd15dfb1bba55a905cbb55bc8557d80
grace-kimm <38813861+grace-kimm@users.noreply.github.com>
2021-03-25 20:21:41 +0900

    210325: section4 ~ 4-3

--- python_coding_test/inflearn/section4/4-2.py
+++ python_coding_test/inflearn/section4/4-2.py
@@ -3,7 +3,7 @@
 # 각 랜선에서 나올 수 있는 짧은 랜선의 수
 def Count(len) :
     cnt = 0
-    for x in Line :
+    for x in Line : # k개
         cnt += (x // len)
     return cnt
 
@@ -19,10 +19,10 @@ for i in range(k) :
 lt = 1
 rt = largest
 while lt <= rt :
-    mid = (lt+rt) // 2
+    mid = (lt+rt) // 2 # 랜선 길이
     if Count(mid) >= n :
         res = mid
-        lt = mid + 1
+        lt = mid + 1 # 더 긴 것도 가능한지 check
     else :
-        rt = mid - 1
+        rt = mid - 1 # 더 짧은 걸로 확인
 print(res)
--- python_coding_test/inflearn/section4/4-3.py
+++ python_coding_test/inflearn/section4/4-3.py
@@ -1,19 +1,19 @@
 def Count(capacity) :
-    # 필요한 dvd 개수
-    cnt = 1
-    # 1장의 dvd 내 누적 된 시간
-    sum = 0
+    cnt = 1 # 필요한 dvd 개수
+    sum = 0 # 1장의 dvd 내 누적 된 시간
     for x in Music :
         if sum + x > capacity :
+            # x곡 저장 불가 -> 새로운 dvd 필요
             cnt += 1
             sum = x
         else :
+            # x곡 저장 가능
             sum += x
     return cnt
 
 n, m = map(int, input().split())
 Music = list(map(int, input().split()))
-maxx = max(Music)
+maxx = max(Music) # dvd 용량은 한 곡의 용량 보단 커야 함
 lt = 1
 rt = sum(Music)
 res = 0
@@ -22,7 +22,7 @@ while lt <= rt :
     mid = (lt + rt) // 2
     if mid >= maxx and Count(mid) <= m :
         res = mid
-        rt = mid - 1
+        rt = mid - 1 # 더 작은 용량으로도 답이 되는지 찾으러 간다.
     else :
-        lt = mid + 1
+        lt = mid + 1 # 더 큰 용량으로 간다.
 print(res)
